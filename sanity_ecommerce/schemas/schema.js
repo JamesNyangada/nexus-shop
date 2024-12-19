a40dbf2497931549aee7e7dad9bf749fb50bef9c
JamesNyangada <jamesedwinjr@hotmail.com>
2024-12-19 14:07:35 +0300

    payment

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -3,10 +3,9 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import product from './product';
 import banner from './banner';
-import order from './order';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([ product, banner, order ]),
+  types: schemaTypes.concat([ product, banner ]),
 })
   
